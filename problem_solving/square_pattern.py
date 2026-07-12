cc8c825dab54c12667ab51728d4455d0017b05a7
aarifakcet <aarifa0912@gmail.com>
2026-07-12 20:58:45 +0530

    sample

--- problem_solving/square_pattern.py
+++ problem_solving/square_pattern.py
@@ -1,2 +1,16 @@
 print("hello world")
 print("hello world")
+''' 
+*****
+*****
+*****
+*****
+*****
+
+'''
+n = 5
+for i in range(n):
+    print("*", end="")
+    print()
+    for j in range(n):
+        print("*", end="")
